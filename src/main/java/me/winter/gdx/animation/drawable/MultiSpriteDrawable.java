d9e1ba8b46035cce617c362c7d20f66f8cf18d25
Alexander Winter <a.w1nter@hotmail.com>
2018-07-21 23:08:05 -0400

    Added multi sprite drawable

---
+++ src/main/java/me/winter/gdx/animation/drawable/MultiSpriteDrawable.java
@@ -0,0 +1,28 @@
+package me.winter.gdx.animation.drawable;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import me.winter.gdx.animation.Sprite;
+
+/**
+ * Draws multiple sprite drawables, one next to the other
+ * <p>
+ * Created on 2018-07-21.
+ *
+ * @author Alexander Winter
+ */
+public class MultiSpriteDrawable implements SpriteDrawable
+{
+	private final SpriteDrawable[] drawables;
+
+	public MultiSpriteDrawable(SpriteDrawable... drawables)
+	{
+		this.drawables = drawables;
+	}
+
+	@Override
+	public void draw(Sprite sprite, Batch batch)
+	{
+		for(SpriteDrawable drawable : drawables)
+			drawable.draw(sprite, batch);
+	}
+}
